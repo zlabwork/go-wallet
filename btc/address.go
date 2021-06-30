00b97fe03d6e2b6aa2c7593ab71294a4c8e61cac
Joe <joe@xxtime.com>
2021-06-30 11:02:05 +0800

    p2wsh

--- btc/address.go
+++ btc/address.go
@@ -51,7 +51,7 @@ func (ad *Address) P2WPKH() string {
 
 // https://github.com/bitcoin/bips/blob/master/bip-0173.mediawiki
 // TODO :: 测试
-func (ad *Address) P2WSH() string {
+func (ad *Address) P2WSH2() string {
     d := append([]byte{OP_PUSH_33}, ad.pub...)
     d = append(d, OP_CHECKSIG)
     ha, _ := utils.HashSha256(d)
@@ -68,6 +68,15 @@ func (ad *Address) P2WSH() string {
         return ""
     }
     return addr
+}
+
+// https://github.com/bitcoin/bips/blob/master/bip-0173.mediawiki
+func (ad *Address) P2WSH() string {
+    r, err := P2WSH([][]byte{ad.pub}, 1, 1)
+    if err != nil {
+        return ""
+    }
+    return r
 }
 
 // P2SH(P2WPKH)
@@ -123,4 +132,40 @@ func P2SHP2WSH(pubKey [][]byte, m, n int) (string, error) {
 
     // P2SH
     return p2sh(hash160), nil
+}
+
+func P2WSH(pubKey [][]byte, m, n int) (string, error) {
+    if m <= 0 || n <= 0 || m > n {
+        return "", fmt.Errorf("error OP_M OP_N")
+    }
+    OP_M := byte(0x50 + m)
+    OP_N := byte(0x50 + n)
+
+    // redeem
+    redeem := []byte{OP_M}
+    for i := 0; i < len(pubKey); i++ {
+        if len(pubKey[i]) != 33 {
+            return "", fmt.Errorf("public key inside P2SH-P2WSH scripts MUST be compressed key")
+        }
+        redeem = append(redeem, OP_PUSH_33)
+        redeem = append(redeem, pubKey[i]...)
+    }
+    redeem = append(redeem, OP_N)
+    redeem = append(redeem, OP_CHECKMULTISIG)
+
+    ha, _ := utils.HashSha256(redeem)
+
+    var program []int
+    for _, i := range ha {
+        program = append(program, int(i))
+    }
+    hrp := "bc"
+    if !isMainNet() {
+        hrp = "tb"
+    }
+    addr, err := bech32.SegwitAddrEncode(hrp, 0, program)
+    if err != nil {
+        return "", nil
+    }
+    return addr, nil
 }

b0abaf90f5e3c42114769fc34a5e36031ab84c2e
Joe <joe@xxtime.com>
2021-06-30 16:33:41 +0800

    doge

---
+++ doge/network.go
@@ -0,0 +1,31 @@
+package doge
+
+var (
+    network = "mainnet"
+)
+
+func SetNetwork(name string) {
+    network = name
+}
+
+func isMainNet() bool {
+    return network == "mainnet"
+}
+
+func getVer(name string) uint8 {
+    switch name {
+    case "P2PKH":
+        if isMainNet() {
+            return 0x1E
+        }
+        return 0x1E
+
+    case "WIF":
+        if isMainNet() {
+            return 0x9E
+        }
+        return 0x9E
+    }
+
+    return 0x00
+}
